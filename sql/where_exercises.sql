636470634cce5e5747c08579b377af3371770d88
brekkenjackson <106278680+brekkenjackson@users.noreply.github.com>
2022-08-29 16:27:45 -0500

    completed where exercises

---
+++ sql/where_exercises.sql
@@ -0,0 +1,10 @@
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY first_name, emp_no DESC;
+SELECT * FROM employees WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name =  'Maya';
+SELECT * FROM employees WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name =  'Maya') AND gender = 'M';
+
+SELECT * FROM employees WHERE last_name LIKE 'E%';
+SELECT * FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%e';
+SELECT * FROM employees WHERE last_name LIKE 'E%e';
+
+SELECT * FROM employees WHERE last_name LIKE '%q%';
+SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
