534c072075c63e9b43f8c2c244314d2081bf5f75
brekkenjackson <106278680+brekkenjackson@users.noreply.github.com>
2022-09-02 10:54:27 -0500

    completed subqueries exercise

---
+++ sql/subqueries_exercises.sql
@@ -0,0 +1,19 @@
+SELECT first_name FROM employees WHERE hire_date IN (
+    SELECT hire_date FROM employees WHERE emp_no = '101010'
+    );
+
+SELECT title FROM titles WHERE emp_no IN (
+    SELECT emp_no FROM employees WHERE first_name = 'Aamod'
+    );
+
+SELECT first_name, last_name FROM employees WHERE emp_no IN (
+    SELECT emp_no FROM dept_manager WHERE gender = 'F' AND to_date = '9999-01-01'
+    );
+
+#BONUS #2
+
+SELECT first_name, last_name FROM employees WHERE emp_no IN (
+    SELECT emp_no FROM salaries WHERE salary IN (
+    SELECT MAX(salary)FROM salaries
+    )
+    );
